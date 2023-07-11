b3818342e3bd474600ce28d5bf038f4c81130911
oukal <Salimoukali@yahoo.com>
2023-07-11 17:34:42 +0200

    Changement controlleur employee >> api

--- src/main/java/com/developer/employeemanagement/controller/EmployeeController.java
+++ src/main/java/com/developer/employeemanagement/controller/EmployeeController.java
@@ -11,7 +11,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/employee")
+@RequestMapping("/api")
 public class EmployeeController {
 
     private final EmployeeService employeeService;
